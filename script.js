5e078da55eb0c2b342575227bdc84c975b694a05
Zac <zacheryngky@gmail.com>
2022-11-02 00:02:53 +0800

    Add displayController module

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ const gameBoard = (() => {
 
     return {getBoard, addMarkerToBoard, isBoardFull};
 })();
+
+// displayController module controls the html elements of the board
+const displayController = (() => {
+    const _gameBoard = document.querySelector(".gameboard");
+    const _loopBoard = (fn) => {
+        for (const cell of _gameBoard.children) {
+            fn(cell);
+        }
+    };
+    const renderBoard = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            _gameBoard.children[i].textContent = arr[i];
+        }
+    };
+    const clearBoard = () => {
+        _loopBoard(el => el.textContent = "");
+    };
+
+    // Listens for clicks on cells   
+    _loopBoard((cell) => cell.addEventListener("click", (e) => console.log(e.target.getAttribute("data-index"))));
+
+    return {renderBoard, clearBoard};
+})();
